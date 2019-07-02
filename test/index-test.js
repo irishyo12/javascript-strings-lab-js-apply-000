1feb4f34e5094eb4957f1108239b7409c076e207
Robert Eggen <rjegrad@yahoo.com>
2019-07-02 17:22:55 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -1,5 +1,5 @@
 /*global concatenatedString, describe, interpolatedString, it, myString */
-
+var greeting = "Hello, everybody!";
 it('defines `greeting`', function() {
   expect(greeting).toEqual("Hello, everybody!")
 })
