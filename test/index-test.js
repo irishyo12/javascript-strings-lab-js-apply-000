b2873d9b775582d9bea4206d2571fba3972cdff9
Robert Eggen <rjegrad@yahoo.com>
2019-07-02 17:26:00 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -3,10 +3,12 @@ var greeting = "Hello, everybody!";
 
 })
 
+
+})
+var greeting = "Hello, everybody!";
 it('concatenates strings to greet a special guest in `greetSpecialGuest`', function() {
   expect(greetSpecialGuest).toEqual(`Hello, ${specialGuest}!`)
 })
 
 it('interpolates a string in `conversation`', function() {
   expect(conversation).toEqual(`Let's talk about ${topic}.`)
-})
